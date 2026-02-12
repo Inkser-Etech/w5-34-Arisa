a3cd48ea52d7e78c658b25259af42947a3250162
Inkser-Etech <Arisa.soprasong@e-tech.ac.th>
2026-02-12 10:10:04 +0700

    fix

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -42,17 +42,37 @@ export default function Add() {
     }
 
     return (
-        <View>
-            <Text>เพิ่มข้อมูล {name} | {price} </Text>
+        <View style={myStyle.container}>
+
+            <View style={myStyle.card}>
+            <Text style={myStyle.title}>เพิ่มข้อมูล</Text>
             {/*ช่องรับชื่อ*/}
-            <TextInput style={myStyle.i} value={name} onChangeText={setName} />
-            {/*ช่องรับราคา*/}
-            <TextInput style={myStyle.i} value={price} onChangeText={setPrice} />
 
-            <TouchableOpacity onPress={addSnack}>
-                <Text>บันทึก</Text>
+            <Text style={myStyle.preview}>
+                    {name} | {price}
+                </Text>
+
+
+            <TextInput 
+            style={myStyle.i} 
+            value={name} 
+            placeholder="ชื่อสินค้า"
+            onChangeText={setName}/>
+           
+            <TextInput
+            style={myStyle.i}
+            value={price}
+             placeholder="ราคา" 
+            onChangeText={setPrice} />
+
+             {/*ช่องรับราคา*/}
+          
+
+
+            <TouchableOpacity style={myStyle.button} onPress={addSnack}>
+                <Text style={myStyle.buttonText}>บันทึก</Text>
             </TouchableOpacity>
-
+        </View>
         </View>
     )
 
@@ -61,5 +81,66 @@ const myStyle = StyleSheet.create({
     i: {
         width: "80%",
         borderWidth: 1,
+        backgroundColor: "#f2f2f2",
+       
+    },
+        container: {
+        flex: 1,
+        backgroundColor: "#f2f2f2",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    card: {
+        width: "90%",
+        
+        backgroundColor: "#fff",
+        padding: 20,
+        borderRadius: 15,
+        elevation: 5, 
+        shadowColor: "#000", 
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10,
+        textAlign: "center"
+    },
+
+    preview: {
+        textAlign: "center",
+        marginBottom: 15,
+        color: "gray",
+        borderRadius:10
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: "#ddd",
+        borderRadius: 10,
+        padding: 12,
+        marginBottom: 15,
+        backgroundColor: "#fafafa"
+    },
+
+    button: {
+        backgroundColor: "#29B6F6",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center"
+    },
+
+    buttonText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16,
+        width: "80%", 
+     alignSelf: "center"
     }
+
 })
